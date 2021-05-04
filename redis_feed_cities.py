74fd39e4337ab30184102f6d9fd991077d01d1df
rohangrge <rohangrge@gmail.com>
2021-05-04 16:15:41 +0530

    update

--- redis_feed_cities.py
+++ redis_feed_cities.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import redis
-r = redis.Redis('192.168.0.133')
+r = redis.Redis()
 states = ['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli', 'Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand',
           'Karnataka', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']
 url = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts'
